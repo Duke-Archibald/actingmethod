8035e341733bff153fe943b92a45726f30de6e09
Duke_Archibald <archibald.addelton@gmail.com>
2022-08-14 13:46:28 -0400

    changes again

--- resources/asd.py
+++ resources/asd.py
@@ -1,6 +1,7 @@
 import sqlite3
 import platform
-print(platform.system())
-conn = sqlite3.connect('../resources/acting_method.db')
-cursor = conn.execute("select ID FROM guilds")
-print([c for c in cursor])
+def test():
+    print(platform.system())
+    conn = sqlite3.connect('./resources/acting_method.db')
+    cursor = conn.execute("select ID FROM guilds")
+    print([c for c in cursor])
